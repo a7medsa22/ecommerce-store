7b700d4dce109378096902c3f8d429a08d274484
a7medsa22 <laslsalsa123@gmail.com>
2025-07-20 00:36:56 +0300

    fix express.raw

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(cors({
 app.use(compression()); // Compress all routes
 app.post(
   "/webhook-checkout",
-  express.json({ type: "application/json" }),
+  express.raw({ type: "application/json" }),
   webhookCheckout
 );
 
